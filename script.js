801ea4fb94ffb133073ff6e21f74db26cb9bb64c
abhishtskoodos <abhishtraghuvanshi@skoodos.com>
2026-02-23 11:20:49 +0530

    Add JavaScript functionality for the agency website

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript functionality for the agency website
+
+// Function to display current date and time
+function displayCurrentDateTime() {
+    const now = new Date();
+    console.log(`Current Date and Time: ${now.toUTCString()}`);
+}
+
+displayCurrentDateTime();
+
+// Add more functionalities as needed....
